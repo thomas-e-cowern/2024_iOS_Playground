b7d42c8e636122f14eeaf1e7c6bb5bc9ddfa68e7
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-06 12:03:07 -0500

    add filtering list project

---
+++ AdvancedSwift/FilteringList/FilteringList/FilteringListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilteringListApp.swift
+//  FilteringList
+//
+//  Created by Thomas Cowern on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FilteringListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
