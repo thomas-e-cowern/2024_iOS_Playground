33eec11f0d07622ef5c40083ac9690d6c4e7b007
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-07 13:55:47 -0500

    add filtering list and update content view to use filtering list generics

---
+++ AdvancedSwift/FilteringList/FilteringList/FilteringList.swift
@@ -0,0 +1,50 @@
+//
+//  FilteringList.swift
+//  FilteringList
+//
+//  Created by Thomas Cowern on 1/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct FilteringList<T: Identifiable, Content: View>: View {
+    
+    @State private var filteredItems = [T]()
+    @State private var filteredString = ""
+    
+    let listItems: [T]
+    let filterKeyPaths: KeyPath<T, String>
+    let content: (T) -> Content
+    
+    var body: some View {
+        VStack {
+            TextField("Type to filter", text: $filteredString.onChange(applyFilter))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            List(filteredItems, rowContent: content)
+                .onAppear(perform: applyFilter)
+        }
+    }
+    
+    init(_ data: [T], filterKeys: KeyPath<T, String>, @ViewBuilder rowContent: @escaping (T) -> Content) {
+        listItems = data
+        filterKeyPaths = filterKeys
+        content = rowContent
+    }
+    
+    func applyFilter() {
+        let cleanedFilter = filteredString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if cleanedFilter.isEmpty {
+            filteredItems = listItems
+        } else {
+            filteredItems = listItems.filter {
+                element in element[keyPath: filterKeyPaths]
+                    .localizedCaseInsensitiveContains(cleanedFilter)
+            }
+        }
+    }
+
+}
