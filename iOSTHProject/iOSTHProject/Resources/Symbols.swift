049a0f3b6f4e7f718d82845f5d56667d90013286
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-16 08:14:20 -0500

    add resources folder, symbols and theme enums, add colorsets to assets

---
+++ iOSTHProject/iOSTHProject/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  iOSTHProject
+//
+//  Created by Thomas Cowern on 1/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ iOSTHProject/iOSTHProject/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  iOSTHProject
+//
+//  Created by Thomas Cowern on 1/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
