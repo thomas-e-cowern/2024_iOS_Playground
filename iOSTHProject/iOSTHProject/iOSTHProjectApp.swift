25883d7a4fa97f67333feff901f187f4cf83e3d3
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-16 07:39:22 -0500

    iOSTHProject initial commit

---
+++ iOSTHProject/iOSTHProject/iOSTHProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTHProjectApp.swift
+//  iOSTHProject
+//
+//  Created by Thomas Cowern on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSTHProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
