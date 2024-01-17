1184c32a92005f22c727cbad074e91a14a874a74
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-17 12:08:38 -0500

    update people view with extracted properties in extensions

--- iOSTHProject/iOSTHProject/Features/People/Views/PeopleView.swift
+++ iOSTHProject/iOSTHProject/Features/People/Views/PeopleView.swift
@@ -14,7 +14,7 @@ struct PeopleView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Theme.background
+                background
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 16) {
                         ForEach(0...5, id: \.self) { item in
@@ -51,13 +51,7 @@ struct PeopleView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button(action: {
-                        
-                    }, label: {
-                        Symbols.plus
-                            .font(.system(.headline, design: .rounded))
-                            .bold()
-                    })
+                    create
                 }
             }
         }
@@ -66,4 +60,20 @@ struct PeopleView: View {
 
 #Preview {
     PeopleView()
+}
+
+extension PeopleView {
+    var create: some View {
+        Button(action: {
+            
+        }, label: {
+            Symbols.plus
+                .font(.system(.headline, design: .rounded))
+                .bold()
+        })
+    }
+    
+    var background: some View {
+        Theme.background
+    }
 }
