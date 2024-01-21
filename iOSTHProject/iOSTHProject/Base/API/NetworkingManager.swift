dc1c479cf659fea2bf9175c4e7b68bc55232be0f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-21 11:04:23 -0500

    add errors to netoworking manager

--- iOSTHProject/iOSTHProject/Base/API/NetworkingManager.swift
+++ iOSTHProject/iOSTHProject/Base/API/NetworkingManager.swift
@@ -15,32 +15,53 @@ final class NetworkingManager {
         
     }
     
-    func request(_ absoluteURL: String) {
-        let url = URL(string: absoluteURL)
-        let request = URLRequest(url: url!)
+    func request<T: Codable>(_ absoluteURL: String, type: T.Type, completion: @escaping (Result<T, Error>) -> Void ) {
+        
+        guard let url = URL(string: absoluteURL) else {
+            completion(.failure(NetworkingError.invalidUrl))
+            return
+        }
+        
+        let request = URLRequest(url: url)
         
         let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             if (error != nil) {
+                completion(.failure(NetworkingError.custom(error: error!)))
                 return
             }
             
             guard let response = response as? HTTPURLResponse, (200...300) ~= response.statusCode else {
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                completion(.failure(NetworkingError.invalidStatusCode(statusCode: statusCode)))
                 return
             }
             
             guard let data = data else {
+                completion(.failure(NetworkingError.invalidData))
                 return
             }
             
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let res = try decoder.decode(UsersResponse.self, from: data)
+                let decodedData = try decoder.decode(T.self, from: data)
+                completion(.success(decodedData))
             } catch {
                 print("Error in networking manager: \(error.localizedDescription)")
+                completion(.failure(NetworkingError.faileToDecode(error: error)))
             }
         }
         
         dataTask.resume()
+    }
+}
+
+extension NetworkingManager {
+    enum NetworkingError: Error {
+        case invalidUrl
+        case custom(error: Error)
+        case invalidStatusCode(statusCode: Int)
+        case invalidData
+        case faileToDecode(error: Error)
     }
 }
