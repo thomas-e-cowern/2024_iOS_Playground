c4fdf8aab045a1e821d7da36ba4c17bd2d1f89d5
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-22 15:12:03 -0500

    add new person model to create/models folder

---
+++ iOSTHProject/iOSTHProject/Features/Create/Models/NewPerson.swift
@@ -0,0 +1,15 @@
+//
+//  NewPerson.swift
+//  iOSTHProject
+//
+//  Created by Thomas Cowern on 1/22/24.
+//
+
+import Foundation
+
+
+struct NewPerson: Codable {
+    var firstName: String = ""
+    var lastName: String = ""
+    var job: String = ""
+}
