ea23db2abe5473ffae4e6773613c2cf81e2de21c
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-22 11:49:05 -0500

    update networking manager with post request

--- iOSTHProject/iOSTHProject/Base/API/NetworkingManager.swift
+++ iOSTHProject/iOSTHProject/Base/API/NetworkingManager.swift
@@ -15,14 +15,14 @@ final class NetworkingManager {
         
     }
     
-    func request<T: Codable>(_ absoluteURL: String, type: T.Type, completion: @escaping (Result<T, Error>) -> Void ) {
+    func request<T: Codable>(methodType: MethodType = .GET, _ absoluteURL: String, type: T.Type, completion: @escaping (Result<T, Error>) -> Void ) {
         
         guard let url = URL(string: absoluteURL) else {
             completion(.failure(NetworkingError.invalidUrl))
             return
         }
         
-        let request = URLRequest(url: url)
+        let request = buildRequest(from: url, methodType: methodType)
         
         let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             if (error != nil) {
@@ -54,7 +54,35 @@ final class NetworkingManager {
         
         dataTask.resume()
     }
+    
+    func request(methodType: MethodType = .POST, _ absoluteURL: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        
+        guard let url = URL(string: absoluteURL) else {
+            completion(.failure(NetworkingError.invalidUrl))
+            return
+        }
+        
+        let request = buildRequest(from: url, methodType: methodType)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            if (error != nil) {
+                completion(.failure(NetworkingError.custom(error: error!)))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...300) ~= response.statusCode else {
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                completion(.failure(NetworkingError.invalidStatusCode(statusCode: statusCode)))
+                return
+            }
+            
+            completion(.success(()))
+        }
+        dataTask.resume()
+    }
 }
+
+
 
 extension NetworkingManager {
     enum NetworkingError: Error {
@@ -63,5 +91,25 @@ extension NetworkingManager {
         case invalidStatusCode(statusCode: Int)
         case invalidData
         case faileToDecode(error: Error)
+    }
+    
+    enum MethodType {
+        case GET
+        case POST
+    }
+}
+
+private extension NetworkingManager {
+    func buildRequest(from url: URL, methodType: MethodType) -> URLRequest {
+        var request = URLRequest(url: url)
+        
+        switch methodType {
+        case .GET:
+            request.httpMethod = "GET"
+        case .POST:
+            request.httpMethod = "POST"
+        }
+        
+        return request
     }
 }
