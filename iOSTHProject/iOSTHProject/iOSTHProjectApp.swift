6e5a8a52e4af312e3886d61471f958247e12b46f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-24 14:24:34 -0500

    update iOSTHProjectApp

--- iOSTHProject/iOSTHProject/iOSTHProjectApp.swift
+++ iOSTHProject/iOSTHProject/iOSTHProjectApp.swift
@@ -17,6 +17,11 @@ struct iOSTHProjectApp: App {
                         Symbols.person
                         Text("Home")
                     }
+                SettingsView()
+                    .tabItem {
+                        Symbols.gear
+                        Text("Settings")
+                    }
             }
         }
     }
