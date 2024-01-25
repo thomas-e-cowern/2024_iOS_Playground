ebb1fcdbe3c1925c03b237c8599ce75a5c59922f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-25 09:56:03 -0500

    add endpoints file

---
+++ iOSTHProject/iOSTHProject/Base/API/Endpoints.swift
@@ -0,0 +1,45 @@
+//
+//  Endpoints.swift
+//  iOSTHProject
+//
+//  Created by Thomas Cowern on 1/25/24.
+//
+
+import Foundation
+
+enum Endpoints {
+    case people
+    case detail(id: Int)
+    case create
+}
+
+extension Endpoints {
+    var host: String { "reqres.in" }
+    
+    var path: String {
+        switch self {
+        case .people,
+                .create:
+            return "/api/users"
+        case .detail(let id):
+            return "/api/users/\(id)"
+        }
+    }
+}
+
+extension Endpoints {
+    var url: URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        
+        #if DEBUG
+        urlComponents.queryItems = [
+            URLQueryItem(name: "delay", value: "1")
+        ]
+        #endif
+        
+        return urlComponents.url
+    }
+}
