29c71e550c89a72eac78d5901c3672e19cfebfe1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-29 16:21:20 -0500

    add error view and error view prievew to content view

--- GlobalErrors/GlobalErrors/ContentView.swift
+++ GlobalErrors/GlobalErrors/ContentView.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+struct ErrorWrapper: Identifiable {
+    let id = UUID()
+    let error: Error
+    let guidancee: String
+}
+
+struct ErrorView: View {
+    
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred")
+                .font(.title)
+                .padding()
+            Text(errorWrapper.error.localizedDescription)
+                .padding(.horizontal, 10)
+            Text(errorWrapper.guidancee)
+                .font(.caption)
+        }
+    }
+}
+
+struct ErrorView_Preview: PreviewProvider {
+    
+    enum simpleError: Error {
+        case opertionFailed
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: ErrorWrapper(error: simpleError.opertionFailed, guidancee: "Please try again..."))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
