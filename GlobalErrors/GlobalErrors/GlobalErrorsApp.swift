ba8f5daaf23afdfcc61845ca160f7dcd47842b80
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-29 16:12:38 -0500

    initial commit for global errors

---
+++ GlobalErrors/GlobalErrors/GlobalErrorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalErrorsApp.swift
+//  GlobalErrors
+//
+//  Created by Thomas Cowern on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GlobalErrorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
