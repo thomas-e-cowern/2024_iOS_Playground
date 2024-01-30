609d3c4bf8e9406e7e29a9f91da65a5d88b27e55
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-30 07:31:33 -0500

    add json mapper tests

---
+++ iOSTHProject/iOSTHProjectTests/Mapper/JSONMapperTests.swift
@@ -0,0 +1,23 @@
+//
+//  JSONMapperTests.swift
+//  iOSTHProjectTests
+//
+//  Created by Thomas Cowern on 1/30/24.
+//
+
+import Foundation
+import XCTest
+
+class JSONMapperTests: XCTestCase {
+    func test_with_valid_json_successfully_decodes() {
+        XCTFail()
+    }
+    
+    func test_with_missing_file_error_throws() {
+        XCTFail()
+    }
+    
+    func test_with_invalid_json_error_throws() {
+        XCTFail()
+    }
+}
