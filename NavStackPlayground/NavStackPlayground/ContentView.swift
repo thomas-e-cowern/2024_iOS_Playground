c3c8db8e0265dbb412ffe6911006b7fefb6abdfb
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-30 13:37:27 -0500

    update content view with start code

--- NavStackPlayground/NavStackPlayground/ContentView.swift
+++ NavStackPlayground/NavStackPlayground/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var platforms: [Platform] = [
+        .init(name: "Xbox", imageName: "xbox.logo", color: .green),
+        .init(name: "Playstation", imageName: "playstation.logo", color: .indigo),
+        .init(name: "PC", imageName: "pc", color: .pink),
+        .init(name: "Mobile", imageName: "iphone", color: .mint)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section("Platofrms") {
+                ForEach(platforms, id: \.name) { platform in
+                    Label(platform.name, systemImage: platform.imageName)
+                        .foregroundColor(platform.color)
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Platform {
+    let name: String
+    let imageName: String
+    let color: Color
 }
