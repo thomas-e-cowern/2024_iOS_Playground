2e7c4e15a75061b4ee2299e8c416060913d5e059
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-30 13:31:59 -0500

    nav stack playground initial commit

---
+++ NavStackPlayground/NavStackPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavStackPlayground
+//
+//  Created by Thomas Cowern on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavStackPlayground/NavStackPlayground/NavStackPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackPlaygroundApp.swift
+//  NavStackPlayground
+//
+//  Created by Thomas Cowern on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NavStackPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
