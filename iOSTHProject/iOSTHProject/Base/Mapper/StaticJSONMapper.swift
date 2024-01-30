fd53b7ef0fb3f614c90b50a4be9f415abca821a1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-30 08:54:28 -0500

    refactor static json mapper to catch empty string

--- iOSTHProject/iOSTHProject/Base/Mapper/StaticJSONMapper.swift
+++ iOSTHProject/iOSTHProject/Base/Mapper/StaticJSONMapper.swift
@@ -9,7 +9,8 @@ import Foundation
 
 struct StaticJSONMapper {
     static func decode<T: Decodable>(file: String, type: T.Type) throws -> T {
-        guard let path = Bundle.main.path(forResource: file, ofType: "json"),
+        guard !file.isEmpty,
+              let path = Bundle.main.path(forResource: file, ofType: "json"),
               let data = FileManager.default.contents(atPath: path) else {
             throw MappingError.failedToGetContents
         }
