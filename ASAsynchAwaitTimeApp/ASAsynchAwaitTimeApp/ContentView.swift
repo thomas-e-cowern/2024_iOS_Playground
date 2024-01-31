1569c0c402c16a114e985c7ee72b2b027b7279dc
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-31 14:55:18 -0500

    update contnet view with task modifier

--- ASAsynchAwaitTimeApp/ASAsynchAwaitTimeApp/ContentView.swift
+++ ASAsynchAwaitTimeApp/ASAsynchAwaitTimeApp/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentDates: [CurrentDate] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List(currentDates, id: \.id) { date in
+                    Text("\(date.date)")
+                }
+                .listStyle(.plain)
+                .navigationTitle("Dates")
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            Task {
+                                await populateDates()
+                            }
+                            
+                        } label: {
+                            Image(systemName: "arrow.clockwise.circle")
+                        }
+                    }
+                }
+            }
+            .padding()
+            .onAppear {
+                Task {
+                    await populateDates()
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func getDate() async throws -> CurrentDate? {
+        guard let url = URL(string: "https://planet-useful-bathroom.glitch.me/current-date") else {
+            fatalError("URL is incorrect")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url) 
+        print("Response: \(response)")
+        print("Data: \(data)")
+        
+        return try JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+    
+    private func populateDates() async {
+        do {
+            guard let currentDate = try await getDate() else {
+                return
+            }
+            
+            self.currentDates.append(currentDate)
+        } catch {
+            print("Error getting dates in ContentView: \(error.localizedDescription)")
+        }
+        
     }
 }
 
