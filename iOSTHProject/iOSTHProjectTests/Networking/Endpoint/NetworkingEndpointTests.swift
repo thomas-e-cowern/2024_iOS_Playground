64a75b9ae4d63beccb96622f2520a193f5ee16c3
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-31 09:20:27 -0500

    add networking and enpoint folders and networking endpoint tests file

---
+++ iOSTHProject/iOSTHProjectTests/Networking/Endpoint/NetworkingEndpointTests.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkingEndpointTests.swift
+//  iOSTHProjectTests
+//
+//  Created by Thomas Cowern on 1/31/24.
+//
+
+import XCTest
+
+final class NetworkingEndpointTests: XCTestCase {
+
+    func test_with_people_endpoint_request_is_valid() {
+        
+    }
+    
+    func test_with_detail_endpoint_request_is_valid() {
+        
+    }
+    
+    func test_with_create_endpoint_request_is_valid() {
+        
+    }
+
+}
