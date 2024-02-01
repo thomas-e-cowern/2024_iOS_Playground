3c1dd986bb556caa68c49cb9cba5bbcb927f69cc
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-01 10:59:24 -0500

    add mocks folder to tests and add mock url session protocol fil

---
+++ iOSTHProject/iOSTHProjectTests/Networking/Mocks/MockUrlSessionProtocol.swift
@@ -0,0 +1,36 @@
+//
+//  MockUrlSessionProtocol.swift
+//  iOSTHProjectTests
+//
+//  Created by Thomas Cowern on 2/1/24.
+//
+
+import Foundation
+import XCTest
+
+class MockUrlSessionProtocol: URLProtocol {
+    
+    static var loadingHandler: (() -> (HTTPURLResponse, Data?))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockUrlSessionProtocol.loadingHandler else {
+            XCTFail("Loading handler is not set")
+            return
+        }
+        
+        let (response, data) = handler()
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        if let data = data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+}
