9fb561c2a05a7fbb0a072a9c26ba6d15cea11ee2
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-02 10:25:41 -0500

    add extensions file(which may have to be renamed) and theme enum

---
+++ ExpenseTracker/ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Thomas Cowern on 2/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("Background")
+    static let text = Color("Text")
+    static let icon = Color("Icon")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
