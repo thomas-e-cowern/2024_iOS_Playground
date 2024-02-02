ddc2e13e5a425eb137f57fad4331e3480d039dd9
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-02 08:19:34 -0500

    add web service

---
+++ ASAsynchAwaitTimeApp/ASAsynchAwaitTimeApp/WebService.swift
@@ -0,0 +1,22 @@
+//
+//  WebService.swift
+//  ASAsynchAwaitTimeApp
+//
+//  Created by Thomas Cowern on 2/2/24.
+//
+
+import Foundation
+
+class WebService {
+    
+    func getDates() async throws -> CurrentDate? {
+        guard let url = URL(string: "https://planet-useful-bathroom.glitch.me/current-date") else {
+            fatalError("URL is incorrect")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        return try JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+    
+}
