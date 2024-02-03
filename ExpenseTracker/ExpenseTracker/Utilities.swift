91638aa5bb35642ccdac0b7b57d64e32a53a8b04
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-03 11:19:36 -0500

    add category extensions and category and subcategory arrays

--- ExpenseTracker/ExpenseTracker/Utilities.swift
+++ ExpenseTracker/ExpenseTracker/Utilities.swift
@@ -49,7 +49,7 @@ extension Category {
     static let publicTransportation = Category (id: 101, name: "Public Transportation", icon: "bus.fill", mainCategoryId: 1)
     static let taxi = Category(id: 102, name: "Taxi", icon: "car.circle.fill", mainCategoryId: 1)
     static let mobilePhone = Category(id: 201, name: "Mobile Phone", icon: "iphone.gen1.circle.fill", mainCategoryId: 2)
-    static let moviesAndbVDs = Category (id: 301, name: "Movies & DVDs", icon: "film.fill", mainCategoryId: 3)
+    static let moviesAndDVDs = Category (id: 301, name: "Movies & DVDs", icon: "film.fill", mainCategoryId: 3)
     static let bankFee = Category (id: 401, name: "Bank Fee", icon: "banknote.fill", mainCategoryId: 4)
     static let financeCharge = Category(id: 402, name: "Finance Charge", icon: "percent", mainCategoryId: 4)
     static let groceries = Category (id: 501, name:"Groceries", icon: "basket.fill", mainCategoryId: 4)
@@ -59,4 +59,36 @@ extension Category {
     static let paycheque = Category(id: 701, name: "Paycheque", icon: "dollarsign.circle.fill", mainCategoryId: 7)
     static let software = Category(id: 801, name: "Software", icon: "desktopcomputer", mainCategoryId: 8)
     static let creditCardPayment = Category(id: 901, name: "creditcard.fill", icon: "", mainCategoryId: 9)
+}
+
+extension Category {
+    
+    static let categories: [Category] = [
+        .autoAndTransport,
+        .billsAndUtilities,
+        .entertainment,
+        .feesAndCharges,
+        .foodAndDining,
+        .home,
+        .income,
+        .shopping,
+        .transfer
+        
+    ]
+    
+    static let subCategories: [Category] = [
+        .publicTransportation,
+        .taxi,
+        .mobilePhone,
+        .moviesAndDVDs,
+        .bankFee,
+        .financeCharge,
+        .groceries,
+        .restaurants,
+        .rent,
+        .homeSupplies,
+        .paycheque,
+        .software,
+        .creditCardPayment
+    ]
 }
