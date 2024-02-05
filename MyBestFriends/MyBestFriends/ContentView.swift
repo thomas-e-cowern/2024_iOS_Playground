1006fd17cf3fa2422ce3316f609a7d05cd0c1e9b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-05 10:31:51 -0500

    my best friends initial commit

---
+++ MyBestFriends/MyBestFriends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBestFriends
+//
+//  Created by Thomas Cowern on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBestFriends/MyBestFriends/MyBestFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBestFriendsApp.swift
+//  MyBestFriends
+//
+//  Created by Thomas Cowern on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBestFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
