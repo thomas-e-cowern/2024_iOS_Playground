a0473b4186927d502e74e23fbee2572b9487a59d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-05 14:50:06 -0500

    update content view with formatting

--- MyBestFriends/MyBestFriends/ContentView.swift
+++ MyBestFriends/MyBestFriends/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(0...19, id: \.self) { item in
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Name")
+                            .font(.system(size: 26, design: .rounded).bold())
+                        
+                        Text("Email")
+                            .font(.callout.bold())
+                        
+                        Text("Phone Number")
+                            .font(.callout.bold())
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .overlay(alignment: .topTrailing) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "star")
+                                .font(.title3)
+                                .symbolVariant(.fill)
+                                .foregroundStyle(Color.gray.opacity(0.3))
+                        })
+                        .buttonStyle(.plain)
+                    }
+                }
+            }
+            .navigationTitle("Contacts")
         }
-        .padding()
     }
 }
-
+        
 #Preview {
     ContentView()
 }
