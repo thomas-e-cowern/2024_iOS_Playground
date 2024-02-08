fec0c8745dfce2146e49663eb3bdb22e6a4f51c1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-08 15:41:33 -0500

    TDNavigationStack initial commit

---
+++ TDNavigationStack/TDNavigationStack/TDNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDNavigationStackApp.swift
+//  TDNavigationStack
+//
+//  Created by Thomas Cowern on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TDNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
