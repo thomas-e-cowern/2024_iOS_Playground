1acb16e9ca27f92bfff5ad16361bac80ff9870db
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-20 12:59:14 -0500

    add sidebar test learning project

---
+++ SidebarTest/SidebarTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SidebarTest
+//
+//  Created by Thomas Cowern on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SidebarTest/SidebarTest/SidebarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarTestApp.swift
+//  SidebarTest
+//
+//  Created by Thomas Cowern on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SidebarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
