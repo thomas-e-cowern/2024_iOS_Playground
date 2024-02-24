356279d62c8823995d7c2c1f97e6fdc72b052e2b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-24 13:24:32 -0500

    add static previews playground

---
+++ StaticPreviewsPlayground/StaticPreviewsPlayground/StaticPreviewsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticPreviewsPlaygroundApp.swift
+//  StaticPreviewsPlayground
+//
+//  Created by Thomas Cowern on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StaticPreviewsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
