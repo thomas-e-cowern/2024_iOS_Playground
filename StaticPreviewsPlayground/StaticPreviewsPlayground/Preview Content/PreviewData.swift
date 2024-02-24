2122af60c24c832a6eef8a2dc44a1e01ab415bbd
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-24 13:37:06 -0500

    add preview data and posts json data to project

---
+++ StaticPreviewsPlayground/StaticPreviewsPlayground/Preview Content/PreviewData.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewData.swift
+//  StaticPreviewsPlayground
+//
+//  Created by Thomas Cowern on 2/24/24.
+//
+
+import Foundation
+
+class PreviewData {
+    static func load<T: Codable>(name: String) -> [T] {
+        if let path = Bundle.main.path(forResource: name, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let results = try JSONDecoder().decode([T].self, from: data)
+                return results
+            } catch {
+                return []
+            }
+        }
+        
+        return []
+    }
+}
