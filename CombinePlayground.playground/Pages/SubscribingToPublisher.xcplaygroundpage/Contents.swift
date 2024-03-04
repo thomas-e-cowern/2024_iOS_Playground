bc48a56ae88410aa35feb449423cd88b049d10f0
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-04 12:18:53 -0500

    play around with cancellables

--- CombinePlayground.playground/Pages/SubscribingToPublisher.xcplaygroundpage/Contents.swift
+++ CombinePlayground.playground/Pages/SubscribingToPublisher.xcplaygroundpage/Contents.swift
@@ -4,12 +4,22 @@ import Foundation
 import UIKit
 import Combine
 
+// Subscription Lifecycles
+let numbersPublisher = (1...10).publisher
 
-// Timer
-let timerPublisher = Timer.publish(every: 1, on: .main, in: .common)
-
-let cancellable = timerPublisher.autoconnect().sink { timestamp in
-    print("Timestamp: \(timestamp)")
+let cancellable = numbersPublisher.sink { value in
+    print(value)
 }
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+    cancellable.cancel()
+}
+
+//// Timer
+//let timerPublisher = Timer.publish(every: 1, on: .main, in: .common)
+//
+//let cancellable = timerPublisher.autoconnect().sink { timestamp in
+//    print("Timestamp: \(timestamp)")
+//}
 
 
