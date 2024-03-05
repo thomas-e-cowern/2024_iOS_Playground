9590eec6f59cb0ebb4359b552654e14bc4b29ab4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-05 11:09:34 -0500

    bird finder initial commit

---
+++ BirdFinder/BirdFinder/BirdFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdFinderApp.swift
+//  BirdFinder
+//
+//  Created by Thomas Cowern on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BirdFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdFinder/BirdFinder/Mapper/StaticJSONMapper.swift
@@ -0,0 +1,8 @@
+//
+//  StaticJSONMapper.swift
+//  BirdFinder
+//
+//  Created by Thomas Cowern on 3/5/24.
+//
+
+import Foundation
