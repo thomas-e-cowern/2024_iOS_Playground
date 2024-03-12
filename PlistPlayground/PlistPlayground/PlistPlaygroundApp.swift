4701c68a9ba6d3de1993e1951d8866b8e957d916
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-12 10:26:44 -0400

    add plist playground

---
+++ PlistPlayground/PlistPlayground/PlistPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlistPlaygroundApp.swift
+//  PlistPlayground
+//
+//  Created by Thomas Cowern on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PlistPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
