e2935da19dac7da7c452f886647694f3167a0c1f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-03-13 09:03:06 -0400

    add linked list page to data-structs-algos

---
+++ Data-Structs-Algos.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+
+import Foundation
+
+class Node<Value> {
+    
+    var value: Value
+    var next: Node?
+    
+    init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    
+    var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
+
+print(node3)
