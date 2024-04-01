e4b58b87f88a4946cced1a8ab933cf45c51d7bb2
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-01 13:43:11 -0400

    update unique letters exercise

--- HWSCC/UniqueLetters.playground/Contents.swift
+++ HWSCC/UniqueLetters.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func challenge1(input: String) -> Bool {
+    
+    var usedLetters = [Character]()
+    
+    let inputArray = Array(input)
+    
+    for letter in inputArray {
+        if usedLetters.contains(letter) {
+            // Note: contains is an O(n) operation and is slow for large strings
+            return false
+        } else {
+            usedLetters.append(letter)
+        }
+    }
+    
+    return true
+}
+
+func challenge1a(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
+challenge1a(input: "AaBbCc")
