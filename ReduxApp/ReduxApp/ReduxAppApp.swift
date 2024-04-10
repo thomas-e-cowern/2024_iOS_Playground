31ac97cea219ad82e4f329ef9153c5e8bc1ef57a
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-10 14:41:01 -0400

    add redux app

---
+++ ReduxApp/ReduxApp/ReduxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxAppApp.swift
+//  ReduxApp
+//
+//  Created by Thomas Cowern on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ReduxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
