5ddff6180354d3e6a18b7a44b48f9f121d748b2e
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-10 09:08:39 -0400

    add greetings app

---
+++ GreetingsApp/GreetingsApp/GreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsAppApp.swift
+//  GreetingsApp
+//
+//  Created by Thomas Cowern on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
