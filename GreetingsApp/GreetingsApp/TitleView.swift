950d3940b244204ea964505f78a5335339510027
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-11 13:26:47 -0400

    update title view with animating ring

--- GreetingsApp/GreetingsApp/TitleView.swift
+++ GreetingsApp/GreetingsApp/TitleView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct TitleView: View {
     
     @State private var subtitle = "Exploaring iOS Programming"
+    @State private var isRotated: Bool = false
+    
+    let lineWidth = 15.0
+    let diameter = 70.0
     
     let subtitles: [String] = [
         "Exploaring iOS Programming",
@@ -18,22 +22,51 @@ struct TitleView: View {
         "A search for employment"
     ]
     
+    var angle: Angle {
+        isRotated ? .zero : .degrees(360)
+    }
+    
+    var angularGradient: AngularGradient {
+        AngularGradient(gradient: Gradient(colors: [.blue, .red, .orange]), center: .center, angle: .zero)
+    }
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("Greetings")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            Text(subtitle)
-                .font(.headline)
-                .fontWeight(.thin)
+        HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Greetings")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text(subtitle)
+                    .font(.headline)
+                    .fontWeight(.thin)
+                
+                
+            }
+            .onTapGesture {
+                // change subtitle
+                subtitle =  subtitles.randomElement() ?? "This is empty..."
+            }
+            
+            Spacer()
+            
+            Circle()
+                .strokeBorder(angularGradient, lineWidth: lineWidth)
+                .rotationEffect(angle)
+                .frame(width: diameter, height: diameter)
+                .onTapGesture {
+                    withAnimation {
+                        isRotated.toggle()
+                    }
+                }
+            
+        
         }
-        .onTapGesture {
-            // change subtitle
-            subtitle =  subtitles.randomElement() ?? "This is empty..."
-        }
+        
+        Spacer()
     }
 }
 
 #Preview {
     TitleView()
+        .padding()
 }
