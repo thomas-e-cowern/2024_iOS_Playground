8e428fe521de743d8f09da022bd8dea01e468ea8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-12 10:28:04 -0400

    add new color set to assets, update background view with new color set

--- GreetingsApp/GreetingsApp/BackgroundView.swift
+++ GreetingsApp/GreetingsApp/BackgroundView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct BackgroundView: View {
     var body: some View {
-        LinearGradient(colors: [.blue, Color(red: 139/255, green: 80/255, blue: 240/255)], startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(colors: [.myBlue, Color(red: 139/255, green: 80/255, blue: 240/255)], startPoint: .topLeading, endPoint: .bottomTrailing)
             .opacity(0.2)
             .ignoresSafeArea()
     }
