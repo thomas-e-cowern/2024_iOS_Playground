7d8050617cede2c2afaec274d7288e044d74f2d1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-16 12:12:21 -0400

    add constants with color constatns to peojct

---
+++ Calculator/Calculator/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by Thomas Cowern on 4/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+// Colors for project
+let buttonBackgroundColor = Color("buttonBackground")
+let darkColor = Color("dark")
+let foregroundDigitsColor = Color("digits")
+let foregroundSideButtonsColor = Color("foregroundSideButtons")
+let foregroundTopButtonsColor = Color("foregroundTopButtons")
+let primaryBackgroundColor = Color("primaryBackground")
+let secondaryBackgroundColor = Color("secondaryBackground")
