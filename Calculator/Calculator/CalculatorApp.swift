adcbec03730cd83e905dded90e8bfd84147e84d9
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-16 11:52:10 -0400

    caculator project initial commit

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Thomas Cowern on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
