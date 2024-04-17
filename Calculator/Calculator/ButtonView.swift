9c297a494ef13b6d73b249232154b1df612ad158
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-17 09:23:38 -0400

    update buttonview with variables

--- Calculator/Calculator/ButtonView.swift
+++ Calculator/Calculator/ButtonView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ButtonView: View {
+    
+    let calculatorButton: CalculatorButton
+    let fgColor: Color
+    let bgColor: Color
+    
+    var systemImage: String? {
+        let value = calculatorButton.rawValue
+        return value.contains("IMG") ? value.replacingOccurrences(of: "IMG", with: "") : nil
+    }
+    
+    var text: String? {
+        let value = calculatorButton.rawValue
+        
+        return value.contains("IMG") ? nil : value
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
+    let buttonDimension: CGFloat = UIScreen.
 }
 
 #Preview {
-    ButtonView()
+    ButtonView(calculatorButton: .one, fgColor: .digits, bgColor: buttonBackgroundColor)
 }
