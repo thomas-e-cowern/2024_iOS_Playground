e0eca0b08e66707ccc976d98e6331da10925f6fb
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-22 14:40:37 -0400

    Alarm project initial commit

---
+++ Alarm/Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
