02ee7c8fb2e2204a5fbad47484e0a00ec5802b71
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-22 15:33:24 -0400

    Add folder structure, add greyed text view to project

---
+++ Alarm/Alarm/Views/Supporting Views/GrayedTextView.swift
@@ -0,0 +1,18 @@
+//
+//  GrayTextView.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 4/22/24.
+//
+
+import SwiftUI
+
+struct GrayedTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GrayedTextView()
+}
