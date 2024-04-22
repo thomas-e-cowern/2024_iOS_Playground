842c4ca4d89c94a32f3db7864b9e53e3fa809ba0
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-22 15:01:20 -0400

    Rename content view to alarm main view, add icon to project

--- Alarm/Alarm/MainAlarmView.swift
+++ Alarm/Alarm/MainAlarmView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainAlarmView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, alarm!")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MainAlarmView()
 }
