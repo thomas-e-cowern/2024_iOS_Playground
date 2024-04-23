ee081d79e9b939bf5f81c5c51626e86e6ec17e28
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-23 08:25:56 -0400

    add time and date support file, add time of day icon view

---
+++ Alarm/Alarm/Time and Date/TimeAndDateSupport.swift
@@ -0,0 +1,8 @@
+//
+//  TimeAndDateSupport.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 4/23/24.
+//
+
+import Foundation
---
+++ Alarm/Alarm/Views/Supporting Views/TimeOfDayIcon.swift
@@ -0,0 +1,18 @@
+//
+//  TimeOfDayIcon.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 4/23/24.
+//
+
+import SwiftUI
+
+struct TimeOfDayIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeOfDayIcon()
+}
