383cd597fcd50960d5d961dce56a9e6dca1acd6f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-23 08:02:15 -0400

    Add color sets to project

---
+++ Alarm/Alarm/Constants/Color Constants.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+let blue = Color("blue")
+let pink = Color("pink")
+let purple = Color("purple")
+let yellow = Color("yellow")
+let darkYellow = Color("darkYellow")
+let lightYellow = Color("lightYellow")
+let backgroundColor = Color("backgroundColor")
+let cardBackgroundColor = Color("cardBackgroundColor")
+let lightGray = Color("lightGray")
+let black = Color("black")
+let nickel = Color("nickel")
+
+let gradColorList = [yellow,pink,purple,blue]
+
+let mainColors = [pink, purple, blue, yellow, lightGray, black]
