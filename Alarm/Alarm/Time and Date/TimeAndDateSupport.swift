0b3c7725d908cf13b1dedcbe9d3bfb87dbb76736
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-23 08:26:13 -0400

    Updated

--- Alarm/Alarm/Time and Date/TimeAndDateSupport.swift
+++ Alarm/Alarm/Time and Date/TimeAndDateSupport.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func dateToPercent(date: Date) -> CGFloat {
+    let result = getTimeComponents(date: date)
+    
+    return CGFloat(result.hour) / 24 + CGFloat(result.minute) / (60 * 24)
+}
+
+func getTimeComponents(date: Date) -> (hour: Int, minute: Int, day: Int, month: Int, year: Int) {
+    let components = Calendar.current.dateComponents([.hour, .minute, .day, .month, .year], from: date)
+    
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+    let day = components.day ?? 0
+    let month = components.month ?? 0
+    let year = components.year ?? 0
+    
+    return (hour: hour, minute: minute, day: day, month: month, year: year)
+}
--- Alarm/Alarm/Views/Supporting Views/TimeOfDayIcon.swift
+++ Alarm/Alarm/Views/Supporting Views/TimeOfDayIcon.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct TimeOfDayIcon: View {
+    
+    // MARK: - Properties
+    let date: Date
+    
+    var percent: CGFloat {
+        return dateToPercent(date: date)
+    }
+    
+    var hour: Int {
+        return Int(24 * percent)
+    }
+    
+    var image: (name: String, color: Color) {
+        switch hour {
+        case 6..<8:
+            return ("sun.horizon.fill", .orange)
+        case 8..<17:
+            return ("sun.max.fill", .yellow)
+        case 17..<20:
+            return ("sun.horizon.fill", .pink)
+        case 20..<6:
+            return ("moon.fill", .black)
+        default:
+            return ("moon.stars.fill", .black)
+        }
+    }
+    
+    // MARK: - Body
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: - Preview
 #Preview {
-    TimeOfDayIcon()
+    TimeOfDayIcon(date: Date())
 }
