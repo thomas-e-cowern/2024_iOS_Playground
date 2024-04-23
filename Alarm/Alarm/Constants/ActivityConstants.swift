9ce25aa6bc5ffd73c6c37124bbeb5fd750d0d651
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-23 12:31:00 -0400

    updated

--- Alarm/Alarm/Constants/ActivityConstants.swift
+++ Alarm/Alarm/Constants/ActivityConstants.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+let activities = [
+    "person.fill",
+    "moon.stars.fill",
+    "moon.zzz.fill",
+    "zzz",
+    "cart",
+    "banknote",
+    "sun.max.fill",
+    "figure.run",
+    "figure.skiing.downhill",
+    "figure.outdoor.cycle",
+    "house",
+    "sofa.fill",
+    "balloon.2",
+    "dumbbell.fill",
+    "sailboat",
+    "scooter"
+]
--- Alarm/Alarm/Model/AlarmModel.swift
+++ Alarm/Alarm/Model/AlarmModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AlarmModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let body: String
+    let repeats: Bool
+    var sounds: String   // MARK: TODO - Define sounds
+    var alarmEnabled: Bool
+    
+    var start: Date
+    var end: Date
+    
+    var activity: String
+    var colorIndex: Int
+    
+    var activityColor: Color {
+        mainColors[colorIndex]
+    }
+    
+    var timeInterval: TimeInterval {
+        end - start
+    }
+    
+    var startTime: TimeModel {
+        dateToTimeMode(date: start)
+    }
+    
+    var endTime: TimeModel {
+        dateToTimeMode(date: end)
+    }
+    
+    static func DefaultAlarm() -> AlarmModel {
+        AlarmModel(
+            title: "Activity completed.",
+            body: "Have a great day!",
+            repeats: false,
+            sounds: .lagrima,
+            alarmEnabled: false,
+            start: Date(),
+            end: Date(),
+            activity: activities[0],
+            colorIndex: 0)
+    }
+}
