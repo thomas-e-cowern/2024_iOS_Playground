eb5aca15fab860f73cf8d874b77c5264859d285d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-24 12:59:36 -0400

    add enable notifications view

---
+++ Alarm/Alarm/Views/Subviews/EnableNotificationsView.swift
@@ -0,0 +1,32 @@
+//
+//  EnableNotificationsView.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 4/24/24.
+//
+
+import SwiftUI
+
+struct EnableNotificationsView: View {
+    var body: some View {
+        ZStack {
+            FourCoolCirclesView()
+            VStack {
+                CoolTextView(text: LocalizedStringKey("Enable notifications, please"), size: 48)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("Notifications Enabled")
+                }, label: {
+                    Text("Enable")
+                })
+            }
+        }
+    }
+}
+
+#Preview {
+    EnableNotificationsView()
+}
