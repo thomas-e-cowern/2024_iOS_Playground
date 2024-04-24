d024faeb67459fa2d1e2c529e78546d251a7e93d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-24 08:54:58 -0400

    add dummy alarm data to alarm model

--- Alarm/Alarm/Model/AlarmModel.swift
+++ Alarm/Alarm/Model/AlarmModel.swift
@@ -50,4 +50,27 @@ struct AlarmModel: Identifiable {
             activity: activities[0],
             colorIndex: 0)
     }
+    
+    static func DummyAlarmData() -> [AlarmModel] {
+        [
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 3, numberOfMinutes: 15), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 20, numberOfMinutes: 30), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 7, numberOfMinutes: 0), activity: "figure.skiing.downhill", colorIndex: 1),
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 20, numberOfMinutes: 7), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 1, numberOfMinutes: 15), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 3, numberOfMinutes: 45), activity: "figure.skiing.downhill", colorIndex: 1),
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 20, numberOfMinutes: 15), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 20, numberOfMinutes: 5), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 22, numberOfMinutes: 00), activity: "figure.skiing.downhill", colorIndex: 1),
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 7, numberOfMinutes: 50), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 18, numberOfMinutes: 35), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 23, numberOfMinutes: 0), activity: "figure.skiing.downhill", colorIndex: 1),
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 10), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 20), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 30), activity: "figure.skiing.downhill", colorIndex: 1),
+            AlarmModel( title: "Activity completed.", body: "Have a great day!", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 10), activity: "moon.zzz.fill", colorIndex: 0),
+            AlarmModel( title: "Yo yo !", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: true, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 20), activity: "sun.max.fill", colorIndex: 2),
+            AlarmModel( title: "Another title", body: "Peace", repeats: false, sounds: .lagrima, alarmEnabled: false, start: Date(), end: addHourToDate(date: Date(), numberOfHours: 0, numberOfMinutes: 30), activity: "figure.skiing.downhill", colorIndex: 1)
+        ]
+    }
 }
