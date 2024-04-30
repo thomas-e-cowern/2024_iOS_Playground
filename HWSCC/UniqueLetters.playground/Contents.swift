49942f59f97bd565e2c15ca6f42c9962ce0d55bc
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-04-30 11:40:16 -0400

    update unique letters

--- HWSCC/UniqueLetters.playground/Contents.swift
+++ HWSCC/UniqueLetters.playground/Contents.swift
@@ -22,4 +22,19 @@ func challenge1a(input: String) -> Bool {
     return Set(input).count == input.count
 }
 
-challenge1a(input: "AaBbCc")
+func challenge1b(input: String) -> Bool {
+    for i in 0..<input.count {
+            for j in (i+1)..<input.count {
+                let firstIndex = input.index(input.startIndex, offsetBy: i)
+                let secondIndex = input.index(input.startIndex, offsetBy: j)
+                if (input[firstIndex] == input[secondIndex]) {
+                    return false
+                }
+            }
+        }
+        return true
+}
+
+//challenge1a(input: "AaBbCc")
+//challenge1(input: "AaBbCcC")
+challenge1b(input: "AaBbCc")
