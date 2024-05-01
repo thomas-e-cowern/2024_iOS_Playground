e01ebea188d993abc8faa350e2b53ad141065d05
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-01 10:03:44 -0400

    add main add edit alarm view, update alarm model end date

---
+++ Alarm/Alarm/Views/Main Views/MainAddEditAlarmView.swift
@@ -0,0 +1,34 @@
+//
+//  MainAddEditAlarmView.swift
+//  Alarm
+//
+//  Created by Thomas Cowern on 5/1/24.
+//
+
+import SwiftUI
+
+struct MainAddEditAlarmView: View {
+    
+    let currentAlarmIndex: Int?
+    @State var alarmModel: AlarmModel
+    @State private var selectedTab = "One"
+    
+    var body: some View {
+        TabView(selection: $selectedTab,
+                content:  {
+            AddEditAlarmView(currentAlarmIndex: currentAlarmIndex, alarmModel: alarmModel).tag("One")
+            AddEditCircularAlarmView(currentAlarmIndex: currentAlarmIndex, alarmModel: alarmModel).tag("Two")
+        })
+        .onAppear() {
+            UIPageControl
+                .appearance()
+                .currentPageIndicatorTintColor = .blue
+        }
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    MainAddEditAlarmView(currentAlarmIndex: nil, alarmModel: .DefaultAlarm())
+}
