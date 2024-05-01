ee638f76b526841b5a5e6402d22cdff80f841671
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-01 09:18:49 -0400

    add 2 ways of checking palindrom

--- HWSCC/Palindrome.playground/Contents.swift
+++ HWSCC/Palindrome.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Never odd or even"
+
+func isPalindrome(str: String) -> Bool {
+    
+//    // One way
+//    let reversedStr = str.reversed()
+//    
+//    if String(reversedStr).lowercased() == str.lowercased() {
+//        return true
+//    }
+//    
+//    return false
+    
+    // another way
+    let lowercased = str.lowercased()
+    
+    return lowercased.reversed() == Array(lowercased)
+}
+
+isPalindrome(str: greeting)
