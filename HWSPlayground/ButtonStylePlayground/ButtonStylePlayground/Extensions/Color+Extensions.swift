bd7ec1ecdfd48b7cc026f9331eb8519218d1a8b3
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-10 14:32:57 -0400

    add extension folder and color extension

---
+++ HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  ButtonStylePlayground
+//
+//  Created by Thomas Cowern on 5/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let classicRed = Color(red: 0.65, green: 0, blue: 0.075)
+    static let classicGreen = Color(red: 0.1, green: 0.6, blue: 0.1)
+    static let classicBlue = Color(red: 0, green: 0.2, blue: 0.7)}
