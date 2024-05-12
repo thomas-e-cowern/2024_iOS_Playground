b04fa84c8889a9c5cb85455fcb562a4871912b47
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-12 09:14:16 -0400

    update aqua button style and content view with aqua button

--- HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/ContentView.swift
+++ HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/ContentView.swift
@@ -9,45 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button {
-            print("pressed")
-        } label: {
-            Text("Press me")
-        }
-        .buttonStyle(EmptyStyle())
-        
-        Button {
-            print("pressed")
-        } label: {
-            Text("Press me")
-        }
-        .buttonStyle(ColoredButtonStyle(color: .green))
-        
-        Button {
-            print("pressed")
-        } label: {
-            Text("Press me")
-        }
-        .buttonStyle(StripedRectangleButtonStyle())
-        
-        Button {
-            print("pressed")
-        } label: {
-            Text("Press")
-        }
-        .buttonStyle(PushButtonStyle())
-        
-        Button {
-            print("pressed")
-        } label: {
-            HStack {
-                Image(systemName: "phone.down.fill")
-                Text("End Call")
+        ScrollView {
+            VStack {
+                Button {
+                    print("pressed")
+                } label: {
+                    Text("Press me")
+                }
+                .buttonStyle(EmptyStyle())
+                
+                Button {
+                    print("pressed")
+                } label: {
+                    Text("Press me")
+                }
+                .buttonStyle(ColoredButtonStyle(color: .green))
+                
+                Button {
+                    print("pressed")
+                } label: {
+                    Text("Press me")
+                }
+                .buttonStyle(StripedRectangleButtonStyle())
+                
+                Button {
+                    print("pressed")
+                } label: {
+                    Text("Press")
+                }
+                .buttonStyle(PushButtonStyle())
+                
+                Button {
+                    print("pressed")
+                } label: {
+                    HStack {
+                        Image(systemName: "phone.down.fill")
+                        Text("End Call")
+                    }
+                }
+                .buttonStyle(GlassButtonStyle(color: .classicRed))
+                .padding(.horizontal)
+                
+                Button {
+                    print("pressed")
+                } label: {
+                    Text("Press Me")
+                        .font(.title)
+                }
+                .buttonStyle(AquaButtonStyle())
             }
+            .padding(.top, 25)
         }
-        .buttonStyle(GlassButtonStyle(color: .classicRed))
-        .padding(.horizontal)
-
     }
 }
 
