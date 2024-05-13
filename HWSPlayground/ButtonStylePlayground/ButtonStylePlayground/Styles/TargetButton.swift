d83e76f275c6064878dff17c4e1ae36e2bcad3b1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-13 10:46:43 -0400

    update target button with formatiing and circle function

--- HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Styles/TargetButton.swift
+++ HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Styles/TargetButton.swift
@@ -13,5 +13,27 @@ struct TargetButton: ButtonStyle {
         configuration.label
             .font(.largeTitle)
             .foregroundColor(.white)
+            .padding(40)
+            .background(
+                RadialGradient(
+                    gradient: Gradient(colors: [Color.blue, Color.blue.opacity(0)]), center: .center, startRadius: 0, endRadius: 50
+                )
+                .opacity(configuration.isPressed ? 0.8 : 1)
+            )
+        
+            .overlay(circle(with: configuration))
+            .overlay(circle(with: configuration).rotationEffect(.init(degrees: 90)))
+            .overlay(circle(with: configuration).rotationEffect(.init(degrees: 180)))
+            .overlay(circle(with: configuration).rotationEffect(.init(degrees: 270)))
+    }
+    
+    func circle(with configuration: Configuration) -> some View {
+        Circle()
+            .trim(from: 0.05, to: 0.2)
+            .stroke(Color.white, lineWidth: 5)
+            .shadow(color: Color.blue, radius: 5)
+            .shadow(color: Color.blue, radius: 5)
+            .shadow(color: Color.blue, radius: 5)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1)
     }
 }
