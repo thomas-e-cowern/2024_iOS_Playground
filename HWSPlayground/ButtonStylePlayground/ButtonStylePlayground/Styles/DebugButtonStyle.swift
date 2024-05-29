b07172125ccce7537c7f83750f1247ee6ecccdc8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-29 12:27:40 -0400

    update debug button style

--- HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Styles/DebugButtonStyle.swift
+++ HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Styles/DebugButtonStyle.swift
@@ -10,4 +10,15 @@ import SwiftUI
 
 struct DebugButtonStyle: PrimitiveButtonStyle {
     
+    let location: String
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            print("Button was pressed on line \(location)")
+            configuration.trigger()
+        } label: {
+            configuration.label
+        }
+        .buttonStyle(DefaultButtonStyle())
+    }
 }
