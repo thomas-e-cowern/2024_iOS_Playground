0e0bfec21c8456cd77f5ce0937b03a36fb4dfab0
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-29 15:06:16 -0400

    add cancellable button style to project

---
+++ HWSPlayground/ButtonStylePlayground/ButtonStylePlayground/Styles/CancellableButtonStyle.swift
@@ -0,0 +1,31 @@
+//
+//  CancellableButtonStyle.swift
+//  ButtonStylePlayground
+//
+//  Created by Thomas Cowern on 5/29/24.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct CancellableButtonStyle: PrimitiveButtonStyle {
+    private struct CancellableButton: View {
+        
+        @State private var timerSubscription: Cancellable?
+        @State private var timer = Timer.publish(every: 1, on: .main, in: .common)
+        @State private var countDown = 0
+        
+        let configuration: Configuration
+        let timeOut: Int
+        
+        var body: some View {
+            Text("Placeholder")
+        }
+    }
+    var timeOut = 3
+    
+    func makeBody(configuration: Configuration) -> some View {
+        CancellableButton(configuration: configuration, timeOut: timeOut)
+    }
+}
