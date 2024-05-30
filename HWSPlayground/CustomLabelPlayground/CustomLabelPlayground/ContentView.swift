f1c43a778204e95a0d7c9277dcfa972ef83dc2a8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-30 09:40:00 -0400

    custom label playground initial commit

---
+++ HWSPlayground/CustomLabelPlayground/CustomLabelPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomLabelPlayground
+//
+//  Created by Thomas Cowern on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HWSPlayground/CustomLabelPlayground/CustomLabelPlayground/CustomLabelPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLabelPlaygroundApp.swift
+//  CustomLabelPlayground
+//
+//  Created by Thomas Cowern on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomLabelPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
