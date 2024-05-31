bf9504cdd62b33eac402d92dbf35c2474212a39b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-31 18:58:39 -0400

    add titled view to project

---
+++ HWSPlayground/TabbedSidebarPlayground/TabbedSidebarPlayground/TitledView.swift
@@ -0,0 +1,15 @@
+//
+//  TitledView.swift
+//  TabbedSidebarPlayground
+//
+//  Created by Thomas Cowern on 5/31/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct TitledView {
+    let title: String
+    let icon: Image
+    let view: AnyView
+}
