96044bcb81bad0ae941a1bd12a78b706338cf24f
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-05-31 13:00:57 -0400

    tabbed bar playground initial commit

---
+++ HWSPlayground/TabbedSidebarPlayground/TabbedSidebarPlayground/TabbedSidebarPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedSidebarPlaygroundApp.swift
+//  TabbedSidebarPlayground
+//
+//  Created by Thomas Cowern on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TabbedSidebarPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
