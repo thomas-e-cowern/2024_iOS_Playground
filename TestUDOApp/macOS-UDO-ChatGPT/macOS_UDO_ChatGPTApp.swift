df8bd6dd4c18d5d1fe9eb31fee1e73c42e642087
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-06-26 14:09:55 -0400

    add macOS-UDO-ChatGPT

---
+++ TestUDOApp/macOS-UDO-ChatGPT/macOS_UDO_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_UDO_ChatGPTApp.swift
+//  macOS-UDO-ChatGPT
+//
+//  Created by Thomas Cowern on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct macOS_UDO_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
