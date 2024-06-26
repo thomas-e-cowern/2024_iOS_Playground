a5aa7cf9fdfa598b26c888198919b59e94122df1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-06-26 14:08:25 -0400

    add iOS-UDO-ChatGPT

---
+++ TestUDOApp/iOS-UDO-ChatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-UDO-ChatGPT
+//
+//  Created by Thomas Cowern on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestUDOApp/iOS-UDO-ChatGPT/iOS_UDO_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_UDO_ChatGPTApp.swift
+//  iOS-UDO-ChatGPT
+//
+//  Created by Thomas Cowern on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_UDO_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
