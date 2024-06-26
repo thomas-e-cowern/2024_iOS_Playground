879389f16e1b1b6a896e51e5362e5988fdba1dae
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-06-26 15:07:16 -0400

    add MainView

---
+++ TestUDOApp/Shared/MainView.swift
@@ -0,0 +1,33 @@
+//
+//  MainView.swift
+//  TestUDOApp
+//
+//  Created by Thomas Cowern on 6/26/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var chatText: String = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            TextField("Search...", text: $chatText)
+                .textFieldStyle(.roundedBorder)
+            Button(action: {
+                //action
+            }, label: {
+                Image(systemName: "paperplane.circle.fill")
+                    .font(.title)
+                    .rotationEffect(Angle(degrees: 45))
+            })
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
