9e3d632347842a2f3867597a5263851bdf818718
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-06-27 09:50:48 -0400

    update plist

--- TestUDOApp/iOS-UDO-ChatGPT/ContentView.swift
+++ TestUDOApp/iOS-UDO-ChatGPT/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("API KEY : \(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "API Unavailable")")
+            
         }
         .padding()
     }
