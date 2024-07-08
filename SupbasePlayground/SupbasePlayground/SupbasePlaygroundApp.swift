f3c5b09e8ab127fb51a4d39efc57b8f9d115ba08
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-08 18:14:37 -0400

    add supabse playground

---
+++ SupbasePlayground/SupbasePlayground/SupbasePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupbasePlaygroundApp.swift
+//  SupbasePlayground
+//
+//  Created by Thomas Cowern on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SupbasePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
