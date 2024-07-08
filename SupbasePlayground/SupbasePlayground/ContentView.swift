3a83b507b88b93a5d6249a1db09a383e88eeb6be
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-08 19:00:38 -0400

    update content view to test connection

--- SupbasePlayground/SupbasePlayground/ContentView.swift
+++ SupbasePlayground/SupbasePlayground/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 import Supabase
 
+struct Budgets: Decodable, Identifiable {
+    var id: Int
+    var name: String
+    var limit: Float
+}
+
 struct ContentView: View {
     
-//    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? "Nothing there"
+    @State var budgets: [Budgets] = []
+    
+    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? "Nothing there"
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        List(budgets) { budget in
+            HStack {
+                Text(budget.name)
+                Text("\(budget.limit)")
+            }
         }
         .padding()
+        .onAppear {
+            
+            connectToSupabase()
+        }
+        .task {
+            do {
+                budgets = try await supabase.from("budgets").select().execute().value
+            } catch {
+                dump(error)
+            }
+        }
+    }
+    
+    func connectToSupabase () {
+        let supabase = SupabaseClient(
+          supabaseURL: URL(string: "https://xwtkxisitnsrfuqvpbld.supabase.co")!,
+          supabaseKey: apiKey
+        )
     }
 }
 
