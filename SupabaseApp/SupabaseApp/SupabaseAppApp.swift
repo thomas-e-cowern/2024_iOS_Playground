4c5cdcdb08374c283855bedb4c5a9f7c76985be4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-09 14:43:51 -0400

    add supabaseapp

---
+++ SupabaseApp/SupabaseApp/SupabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseAppApp.swift
+//  SupabaseApp
+//
+//  Created by Thomas Cowern on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
