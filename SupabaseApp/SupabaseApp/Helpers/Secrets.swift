3b1eb484120155ce27f6ac014b4c102116ea77b6
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-09 15:30:18 -0400

    add helpers folder, update view and secrets file

---
+++ SupabaseApp/SupabaseApp/Helpers/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  SupabaseApp
+//
+//  Created by Thomas Cowern on 7/9/24.
+//
+
+import Foundation
+
+enum Secrets {
+    static let projectURL = URL(string: "")!
+        
+    static let apiKey = ""
+}
