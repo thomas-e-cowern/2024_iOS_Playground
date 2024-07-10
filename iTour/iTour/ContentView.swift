0200a29025bca10ab68220d56d20f619716b35a4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-10 16:21:08 -0400

    add EditDestinationView, update content view with addDestination function and path variable

--- iTour/iTour/ContentView.swift
+++ iTour/iTour/ContentView.swift
@@ -11,22 +11,28 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext)  var modelContext
     @Query var destinations: [Destination]
+    @State private var path = [Destination]()
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(destinations) { destination in
-                    VStack(alignment: .leading, content: {
-                        Text(destination.name)
-                            .font(.headline)
-                        
-                        Text(destination.date.formatted(date: .long, time: .shortened))
-                    })
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading, content: {
+                            Text(destination.name)
+                                .font(.headline)
+                            
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        })
+                    }
                 }
+                .onDelete(perform: deleteDestination)
             }
             .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
             .toolbar {
                 Button("Add Samples", action: addSamples)
+                Button("Add Destination", action: addDestination)
             }
         }
     }
@@ -40,6 +46,19 @@ struct ContentView: View {
         modelContext.insert(fortlauderdale)
         modelContext.insert(orlando)
         
+    }
+    
+    func deleteDestination(_ indexsSet: IndexSet) {
+        for index in indexsSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
     }
 }
 
