31901b659d2139b9aebd015b97d0fd92db6baf15
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-10 14:10:03 -0400

    add destination model, update content view to add and show sample data

--- iTour/iTour/ContentView.swift
+++ iTour/iTour/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext)  var modelContext
+    @Query var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading, content: {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    })
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let miami = Destination(name: "Miami")
+        let fortlauderdale = Destination(name: "Fort Lauderdale")
+        let orlando = Destination(name: "Orlando")
+        
+        modelContext.insert(miami)
+        modelContext.insert(fortlauderdale)
+        modelContext.insert(orlando)
+        
     }
 }
 
