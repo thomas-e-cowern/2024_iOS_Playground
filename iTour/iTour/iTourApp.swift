fff644713344de403c0f6ab4d8444d0730fded1a
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-10 13:48:14 -0400

    iTour initial commit

---
+++ iTour/iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Thomas Cowern on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
