e973afb7b491f46a78caccede4f355d25aea73a5
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-18 08:34:29 -0400

    add variable, format view for questions in QuestionView

--- QuizApp/QuizApp/Views/QuestionView.swift
+++ QuizApp/QuizApp/Views/QuestionView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct QuestionView: View {
+    
+    @State var question = Question(title: "When was the iPhone first released?", answer: "A", choices: ["A", "B", "C", "D"])
+    
     var body: some View {
-        VStack {
-            Text("When was the iPhone first released")
+        VStack(alignment: .leading) {
+            Text(question.title)
+                .padding(.bottom)
+                .font(.headline)
+            
+            ForEach(question.choices, id: \.self) { choice in
+                Text(choice)
+            }
         }
+        .padding(.horizontal)
+        .frame(height: 550)
+        .background(Color(uiColor: .systemGray6))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(color: Color(uiColor: .label).opacity(0.2), radius: 15)
     }
 }
 
