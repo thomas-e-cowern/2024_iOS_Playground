e8dd0468be95dfaf730aaafc89e3b6f4f1de367b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-18 09:32:23 -0400

    add managers folder and QuizManager class

---
+++ QuizApp/QuizApp/Managers/QuizManager.swift
@@ -0,0 +1,18 @@
+//
+//  QuizManager.swift
+//  QuizApp
+//
+//  Created by Thomas Cowern on 7/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+class QuizManager: ObservableObject {
+    var mockQuestions = [
+       Question(title: "When was the iPhone first released?", answer: "A", choices: ["A", "B", "C", "D"]),
+       Question(title: "When was the iPhone first released?", answer: "A", choices: ["A", "B", "C", "D"]),
+       Question(title: "When was the iPhone first released?", answer: "A", choices: ["A", "B", "C", "D"]),
+       Question(title: "When was the iPhone first released?", answer: "A", choices: ["A", "B", "C", "D"])
+    ]
+}
