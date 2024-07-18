d095e2277d6a95e6ab3fc91fbc1f393da43fc8cf
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-18 08:25:03 -0400

    add models folder, add Question model

---
+++ QuizApp/QuizApp/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Thomas Cowern on 7/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Question {
+    let title: String
+    let answer: String
+    let choices: [String]
+}
