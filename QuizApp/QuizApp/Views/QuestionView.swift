ff6ec30072d68f0c8f7796e37adc8e75106f32fc
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-18 08:23:02 -0400

    add views folder, add QuestionView

---
+++ QuizApp/QuizApp/Views/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  QuizApp
+//
+//  Created by Thomas Cowern on 7/18/24.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack {
+            Text("When was the iPhone first released")
+        }
+    }
+}
+
+#Preview {
+    QuestionView()
+}
