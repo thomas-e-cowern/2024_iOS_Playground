8b22b752edf112cd810e9c46e9421fd19022a4e5
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-24 10:24:14 -0400

    SwiftDataCloudkitSync initial commit

---
+++ SwiftDataCloudkitSync/SwiftDataCloudkitSync/SwiftDataCloudkitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCloudkitSyncApp.swift
+//  SwiftDataCloudkitSync
+//
+//  Created by Thomas Cowern on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCloudkitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
