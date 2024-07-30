34914c825122a968e7959323303aa13b0594c25c
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-07-30 10:19:44 -0400

    group box playground initial commit

---
+++ GroupBoxPlayground/GroupBoxPlayground/GroupBoxPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxPlaygroundApp.swift
+//  GroupBoxPlayground
+//
+//  Created by Thomas Cowern on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
