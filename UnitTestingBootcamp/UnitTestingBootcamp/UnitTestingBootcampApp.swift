2f24f6abb350de890235a963750519299d20f8c8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-07 08:53:27 -0400

    UnitTestingBootcamp initial commit

---
+++ UnitTestingBootcamp/UnitTestingBootcamp/UnitTestingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingBootcampApp.swift
+//  UnitTestingBootcamp
+//
+//  Created by Thomas Cowern on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
