5c4bf9cc0f25eefd4d9a41ed9bd26ed95b17374d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-10 15:20:12 -0400

    SwiftUIDesignPatterns

---
+++ SwiftUIDesignPatterns/SwiftUIDesignPatterns/SwiftUIDesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignPatternsApp.swift
+//  SwiftUIDesignPatterns
+//
+//  Created by Thomas Cowern on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserListScreen()
+        }
+    }
+}
---
+++ SwiftUIDesignPatterns/SwiftUIDesignPatterns/UserListScreen.swift
@@ -0,0 +1,24 @@
+//
+//  UserListScreen.swift
+//  SwiftUIDesignPatterns
+//
+//  Created by Thomas Cowern on 8/10/24.
+//
+
+import SwiftUI
+
+struct UserListScreen: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    UserListScreen()
+}
