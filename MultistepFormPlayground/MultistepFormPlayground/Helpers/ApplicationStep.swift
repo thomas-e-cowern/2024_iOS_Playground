2eba83a3aa4a49e3a6804d389aaac48cc001c680
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-22 10:21:24 -0400

    add ApplicationSetp enum to Helpers folder

---
+++ MultistepFormPlayground/MultistepFormPlayground/Helpers/ApplicationStep.swift
@@ -0,0 +1,15 @@
+//
+//  ApplicationStep.swift
+//  MultistepFormPlayground
+//
+//  Created by Thomas Cowern on 8/22/24.
+//
+
+import Foundation
+
+enum ApplicationStep {
+    case personal
+    case damage
+    case address
+    case review
+}
