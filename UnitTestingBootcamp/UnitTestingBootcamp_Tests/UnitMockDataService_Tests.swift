71544f9fcdc3508b64d247a6fe0a5c4617e5f70a
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-29 15:08:52 -0400

    add test_NewMockDataService_downloadItemsWithCombine_doesReturnValues to UnitMockDataService_Tests

--- UnitTestingBootcamp/UnitTestingBootcamp_Tests/UnitMockDataService_Tests.swift
+++ UnitTestingBootcamp/UnitTestingBootcamp_Tests/UnitMockDataService_Tests.swift
@@ -10,6 +10,8 @@ import XCTest
 import Combine
 
 final class UnitMockDataService_Tests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,16 +19,71 @@ final class UnitMockDataService_Tests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        cancellables.removeAll()
     }
     
     func test_NewMockDataService_init_doesSetValuesCorrectly() {
         // Given
         let items: [String]? = nil
+        let items2: [String]? = []
+        let items3: [String]? = [UUID().uuidString, UUID().uuidString]
         
         // When
         let dataService = NewMockDataService(items: items)
+        let dataService2 = NewMockDataService(items: items2)
+        let dataService3 = NewMockDataService(items: items3)
 
         // Then
         XCTAssertFalse(dataService.items.isEmpty)
+        XCTAssertTrue(dataService2.items.isEmpty)
+        XCTAssertEqual(dataService3.items.count, 2)
+    }
+    
+    func test_NewMockDataService_downloadItemsWithEscaping_doesReturnValues() {
+        // Given
+        let dataService = NewMockDataService(items: nil)
+        
+        // When
+        var items: [String] = []
+        
+        let expectation = XCTestExpectation()
+        
+        dataService.downloadItemWithEscaping { returnedItems in
+            items = returnedItems
+            expectation.fulfill()
+        }
+
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataService.items.count)
+    }
+    
+    func test_NewMockDataService_downloadItemsWithCombine_doesReturnValues() {
+        // Given
+        let dataService = NewMockDataService(items: nil)
+        
+        // When
+        var items: [String] = []
+        
+        let expectation = XCTestExpectation()
+        
+        dataService.downloadItemsWithCombine()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case.failure(let error):
+                    XCTFail()
+                }
+            } receiveValue: { returnedItems in
+                items = returnedItems
+                
+            }
+            .store(in: &cancellables)
+
+
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataService.items.count)
     }
 }
