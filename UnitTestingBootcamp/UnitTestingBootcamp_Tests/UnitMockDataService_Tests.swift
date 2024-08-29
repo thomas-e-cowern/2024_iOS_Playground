9e3aeed8735aa3ebf837629491643cd411dc1fae
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-29 13:59:32 -0400

    add test_NewMockDataService_init_doesSetValuesCorrectly to UnitMockDataService_Tests

--- UnitTestingBootcamp/UnitTestingBootcamp_Tests/UnitMockDataService_Tests.swift
+++ UnitTestingBootcamp/UnitTestingBootcamp_Tests/UnitMockDataService_Tests.swift
@@ -6,6 +6,8 @@
 //
 
 import XCTest
+@testable import UnitTestingBootcamp
+import Combine
 
 final class UnitMockDataService_Tests: XCTestCase {
 
@@ -16,20 +18,15 @@ final class UnitMockDataService_Tests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test_NewMockDataService_init_doesSetValuesCorrectly() {
+        // Given
+        let items: [String]? = nil
+        
+        // When
+        let dataService = NewMockDataService(items: items)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Then
+        XCTAssertFalse(dataService.items.isEmpty)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
