cc239c16b21bbcbb4005759950f8adb99a60ffe6
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-08-30 09:50:55 -0400

    add UITestingBootcamp

---
+++ UITestingBootcamp/UITestingBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITestingBootcamp
+//
+//  Created by Thomas Cowern on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UITestingBootcamp/UITestingBootcamp/UITestingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingBootcampApp.swift
+//  UITestingBootcamp
+//
+//  Created by Thomas Cowern on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct UITestingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
