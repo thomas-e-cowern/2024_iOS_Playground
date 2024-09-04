dd51e95d40f52de3353e049b70afcaed01efc9fe
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-04 18:10:58 -0400

    update NavigationAction with enums

--- NavigationPlayground/NavigationPlayground/NavigationAction.swift
+++ NavigationPlayground/NavigationPlayground/NavigationAction.swift
@@ -5,7 +5,7 @@
 //  Created by Thomas Cowern on 9/4/24.
 //
 
-import Foundation
+import SwiftUI
 
 struct NavigationAction {
     
@@ -14,12 +14,41 @@ struct NavigationAction {
 enum PatientRoute: Hashable {
     case list
     case create
-    case detail(patient)
+    case detail(Patient)
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .list:
+            Text("PatientRoute list")
+        case .create:
+            Text("PatientRoute create")
+        case .detail(let patient):
+            Text("Patient detail for \(patient.name)")
+        }
+    }
 }
 
+enum DoctorRoute: Hashable {
+    case list
+    case create
+    case detail(Doctor)
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .list:
+            Text("DoctorRoute list")
+        case .create:
+            Text("DoctorRoute create")
+        case .detail(let doctor):
+            Text("Doctor detail for \(doctor.name)")
+        }
+    }
+}
+
+
 enum Route: Hashable {
-    case patientList
-    case patientAdd
-    case patientDetail
-    case doctor
+    case patient(PatientRoute)
+    case doctor(DoctorRoute)
 }
