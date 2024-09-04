4dce08935711220b51024577ac964db862327c29
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-04 17:59:19 -0400

    add NavigationPlayground

---
+++ NavigationPlayground/NavigationPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationPlayground
+//
+//  Created by Thomas Cowern on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationPlayground/NavigationPlayground/NavigationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPlaygroundApp.swift
+//  NavigationPlayground
+//
+//  Created by Thomas Cowern on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
