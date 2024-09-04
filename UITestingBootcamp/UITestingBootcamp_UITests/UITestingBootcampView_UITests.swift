a8952fc63838e1a90e2b9a2a6c2fb001412daa13
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-04 03:49:20 -0400

    add UITestingBootcampView_UITests to  UITestingBootcamp

---
+++ UITestingBootcamp/UITestingBootcamp_UITests/UITestingBootcampView_UITests.swift
@@ -0,0 +1,59 @@
+//
+//  UITestingBootcampView_UITests.swift
+//  UITestingBootcamp_UITests
+//
+//  Created by Thomas Cowern on 8/30/24.
+//
+
+import XCTest
+
+// Name Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+// Name Structure: test_[struct or class]_[variable or function or UIComponent]_[expected result]
+
+// Testing Structure: Given, When, Then
+
+final class UITestingBootcampView_UITests: XCTestCase {
+    
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_UITestingBootcampView_signUpButton_shouldNotSignIn() {
+        
+    }
+    
+    func test_UITestingBootcampView_signUpButton_shouldSignIn() {
+        
+        // Given
+        let textField = app.textFields["Add your name..."]
+        
+        // When
+        textField.tap()
+        
+        let bKey = app/*@START_MENU_TOKEN@*/.keys["B"]/*[[".keyboards.keys[\"B\"]",".keys[\"B\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        bKey.tap()
+        
+        let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        oKey.tap()
+
+        let bKey2 = app/*@START_MENU_TOKEN@*/.keys["b"]/*[[".keyboards.keys[\"b\"]",".keys[\"b\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        bKey2.tap()
+
+        let signupButton = app.buttons["Sign Up"]
+        signupButton.tap()
+        
+        let welcomeText = app.navigationBars["Welcome"].staticTexts["Welcome"]
+        
+        // Then
+        XCTAssertTrue(welcomeText.exists)
+
+    }
+}
