b885483e4dcf112609479d0aa2f6d2155450951e
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-05 14:06:48 -0400

    HWSNavigationStack initial commit

---
+++ HWSPlayground/HWSNavigationStack/HWSNavigationStack/HWSNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSNavigationStackApp.swift
+//  HWSNavigationStack
+//
+//  Created by Thomas Cowern on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HWSNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
