059f207db8ad4250495a7610c026a9ff5630f879
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-05 10:44:27 -0400

    NavigationStackPlayground initial commit

---
+++ NavigationStackPlayground/NavigationStackPlayground/NavigationStackPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackPlaygroundApp.swift
+//  NavigationStackPlayground
+//
+//  Created by Thomas Cowern on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
