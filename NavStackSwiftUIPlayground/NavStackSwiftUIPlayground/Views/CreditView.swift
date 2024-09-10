c0a10cb7a9aefce400189281f5c25f73b4657f22
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-10 15:11:09 -0400

    add all views to project

--- NavStackSwiftUIPlayground/NavStackSwiftUIPlayground/Views/CreditView.swift
+++ NavStackSwiftUIPlayground/NavStackSwiftUIPlayground/Views/CreditView.swift
@@ -1,5 +1,5 @@
 //
-//  CharacterView.swift
+//  CreditView.swift
 //  NavStackSwiftUIPlayground
 //
 //  Created by Thomas Cowern on 9/10/24.
@@ -7,33 +7,26 @@
 
 import SwiftUI
 
-struct CharacterView: View {
-    
+struct CreditView: View {
     @ObservedObject var characterState = HarryPotterCharactersState.shared
     @Binding var navPaths: [Routes]
     
     var body: some View {
-        VStack {
-            Text(characterState.currentCharacter.name).font(.title2).padding(.bottom, 10)
-            Image(characterState.currentCharacter.image)
-                .resizable()
-                .frame(width: 300, height: 300)
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color(UIColor.systemGray2), lineWidth: 4)
-                )
-                .padding(.bottom, 10)
+        VStack{
+            Text("\(characterState.currentCharacter.name)'s Credits").font(.system(size: 22, weight: .bold)).padding(.top, 20)
             
-            Text(characterState.currentCharacter.summary)
-                .font(.body)
-                .multilineTextAlignment(.leading)
-                .padding()
+            VStack(alignment: .center)  {
+                ForEach(characterState.currentCharacter.credits, id: \.self) { credit in
+                    Text("\(credit)").font(.title2).padding(10)
+                }
+                
+            }
             
-            Button("See Credits") {
-                //navigate to credits
-                navPaths.append(.credits)
+            Spacer()
+            
+            Button("Back to Character") {
+                // navigate back
+                navPaths.removeLast()
             }
             .font(.title3)
             .frame(width: 200)
@@ -44,9 +37,9 @@ struct CharacterView: View {
             .padding(.bottom, 30)
             
             
-            Button("View Quotes") {
-                // navigate to quotes
-                navPaths.append(.quotes)
+            Button("Back to Home Screen") {
+                // navigate to home screen
+                navPaths.removeAll()
             }
             .font(.title3)
             .frame(width: 200)
@@ -55,20 +48,21 @@ struct CharacterView: View {
             .foregroundColor(.primary)
             .cornerRadius(8)
             
+            Spacer()
+            
         }.padding(20)
             .navigationBarItems(
                 leading:  Image(systemName: "arrow.left.circle.fill").resizable().frame(width: 32, height: 32).onTapGesture {
                     // navigate back
                     navPaths.removeLast()
-                    
-        
                 },
                 trailing: Text("Potter Verse").font(.title3)
             )
-           
+        
+        
     }
 }
 
 #Preview {
-    CharacterView(navPaths: .constant([]))
+    CreditView(navPaths: .constant([]))
 }
--- NavStackSwiftUIPlayground/NavStackSwiftUIPlayground/Views/QuoteView.swift
+++ NavStackSwiftUIPlayground/NavStackSwiftUIPlayground/Views/QuoteView.swift
@@ -1,5 +1,5 @@
 //
-//  CharacterView.swift
+//  QuoteView.swift
 //  NavStackSwiftUIPlayground
 //
 //  Created by Thomas Cowern on 9/10/24.
@@ -7,33 +7,24 @@
 
 import SwiftUI
 
-struct CharacterView: View {
-    
+struct QuoteView: View {
     @ObservedObject var characterState = HarryPotterCharactersState.shared
     @Binding var navPaths: [Routes]
     
     var body: some View {
+        
         VStack {
-            Text(characterState.currentCharacter.name).font(.title2).padding(.bottom, 10)
-            Image(characterState.currentCharacter.image)
-                .resizable()
-                .frame(width: 300, height: 300)
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color(UIColor.systemGray2), lineWidth: 4)
-                )
-                .padding(.bottom, 10)
+            Text("\(characterState.currentCharacter.name)'s Famous Quotes").font(.system(size: 22, weight: .bold)).padding(.top, 20)
             
-            Text(characterState.currentCharacter.summary)
-                .font(.body)
-                .multilineTextAlignment(.leading)
-                .padding()
+            ForEach(characterState.currentCharacter.quotes, id: \.self) { quote in
+                Text("\"\(quote)\"").font(.title2).padding(20)
+            }
             
-            Button("See Credits") {
-                //navigate to credits
-                navPaths.append(.credits)
+            Spacer()
+            
+            Button("Back to Character") {
+               //navigate back
+               navPaths.removeLast()
             }
             .font(.title3)
             .frame(width: 200)
@@ -44,9 +35,9 @@ struct CharacterView: View {
             .padding(.bottom, 30)
             
             
-            Button("View Quotes") {
-                // navigate to quotes
-                navPaths.append(.quotes)
+            Button("Back to Home Screen") {
+                //navigate to home screen
+                navPaths.removeAll()
             }
             .font(.title3)
             .frame(width: 200)
@@ -55,20 +46,21 @@ struct CharacterView: View {
             .foregroundColor(.primary)
             .cornerRadius(8)
             
+            Spacer()
+            
         }.padding(20)
             .navigationBarItems(
                 leading:  Image(systemName: "arrow.left.circle.fill").resizable().frame(width: 32, height: 32).onTapGesture {
-                    // navigate back
+                    //navigate back
                     navPaths.removeLast()
-                    
-        
+                
                 },
                 trailing: Text("Potter Verse").font(.title3)
             )
-           
+        
     }
 }
 
 #Preview {
-    CharacterView(navPaths: .constant([]))
+    QuoteView(navPaths: .constant([]))
 }
