107e8acd620a4d08711c07c2a89f739b253290c9
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-10 12:51:11 -0400

    NavStackSwiftUIPlayground initial commit

---
+++ NavStackSwiftUIPlayground/NavStackSwiftUIPlayground/NavStackSwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackSwiftUIPlaygroundApp.swift
+//  NavStackSwiftUIPlayground
+//
+//  Created by Thomas Cowern on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NavStackSwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
