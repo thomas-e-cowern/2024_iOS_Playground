33a64f012240bcbd022f25cc99c066823c585108
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-12 08:34:54 -0400

    ShortDurationView initial commit

---
+++ ShortDurationView/ShortDurationView/ShortDurationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortDurationViewApp.swift
+//  ShortDurationView
+//
+//  Created by Thomas Cowern on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ShortDurationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
