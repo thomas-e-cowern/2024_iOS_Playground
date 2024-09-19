7fd8c7889446dd931e424770f2316b2aa2efa90d
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-19 14:17:02 -0400

    add Views folder, move ContentView to Views folder, add MessageListView to Views folder

---
+++ ChatApp/ChatApp/Views/MessageListView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageListView.swift
+//  ChatApp
+//
+//  Created by Thomas Cowern on 9/19/24.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageListView()
+}
