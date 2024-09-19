15c813a324240024207d82da18f89ee3cd8a188b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-19 11:24:42 -0400

    ChatApp initial commit

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Thomas Cowern on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApp/ChatAppTests/ChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppTests.swift
+//  ChatAppTests
+//
+//  Created by Thomas Cowern on 9/19/24.
+//
+
+import Testing
+@testable import ChatApp
+
+struct ChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
