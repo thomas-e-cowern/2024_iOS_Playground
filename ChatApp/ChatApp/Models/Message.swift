5e1c1da8a0c1fb07c836451d66626bce4e843ea1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-20 16:00:48 -0400

    add Models folder and Message model

---
+++ ChatApp/ChatApp/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Thomas Cowern on 9/20/24.
+//
+
+import Foundation
+
+struct Message {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
