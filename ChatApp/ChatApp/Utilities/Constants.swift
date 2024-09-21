774bcfce3293705b4f622f3b8f54f23c09148850
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-09-21 10:58:44 -0400

    add Utilities folder and Constants file to project

---
+++ ChatApp/ChatApp/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Thomas Cowern on 9/21/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrlPath = "https://jsonplaceholder.typicode.com"
+    
+    struct Urls {
+        
+        static let posts = URL(string: "\(baseUrlPath)/posts")
+    }
+}
