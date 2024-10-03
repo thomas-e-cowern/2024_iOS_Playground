784277957509c5d7e37fd112284c797f66b0add8
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-03 12:20:23 -0400

    update RemovingOptionals in HWSCareerAccelerator

--- HWSCareerAccelerator/HWSCareerAccelerator/RemovingOptionals.swift
+++ HWSCareerAccelerator/HWSCareerAccelerator/RemovingOptionals.swift
@@ -7,19 +7,31 @@
 
 import SwiftUI
 
+enum PasswordError: Error {
+    case obvious(detailedError: String)
+}
+
 struct RemovingOptionals: View {
     
     let numberStrings = ["1", "2", "Three"]
-    let urlStrings = ["https://apple.com", "https://swift.org", ""]
+    let urlStrings = ["https://apple.com", "https://swift.org", "", nil]
     let maybeNumbers = [1, nil, 2, nil, 3]
     @State var numberInts: [Int] = []
     @State var urls: [URL] = []
     @State var definitelyNumbers: [Int] = []
+    @State var encryptResult: String = ""
     
     var body: some View {
         VStack {
             Button("Map Me!") {
                 mapMe()
+                
+                do {
+                    try encryptResult = encrypt(key: "12345") ?? "Failed Encryption"
+                } catch {
+                    print(error)
+                }
+                
             }
             Spacer()
                 .frame(height: 20)
@@ -40,13 +52,24 @@ struct RemovingOptionals: View {
                     Text("\(number)")
                 }
             }
+            .frame(height: 20)
+            Text(encryptResult)
         }
     }
     
     func mapMe() {
         numberInts = numberStrings.compactMap { Int($0) }
-        urls = urlStrings.compactMap { URL(string: $0) }
+        urls = urlStrings.compactMap { URL(string: $0 ?? "") }
         definitelyNumbers = maybeNumbers.compactMap { $0 }
+    }
+    
+    func encrypt(key: String) throws -> String? {
+        if key == "12345" {
+            print("I have the same key on my luggage.")
+            throw PasswordError.obvious(detailedError: "I have the same key on my luggage.")
+        }
+
+        return "Encryption complete"
     }
 }
 
