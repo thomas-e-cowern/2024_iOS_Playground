f68a053e0becaaebc4dbf840547dfca392e14c00
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-03 11:19:01 -0400

    add RemovingOptionals to HWSCareerAccelerator

---
+++ HWSCareerAccelerator/HWSCareerAccelerator/RemovingOptionals.swift
@@ -0,0 +1,18 @@
+//
+//  RemovingOptionals.swift
+//  HWSCareerAccelerator
+//
+//  Created by Thomas Cowern on 10/3/24.
+//
+
+import SwiftUI
+
+struct RemovingOptionals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemovingOptionals()
+}
