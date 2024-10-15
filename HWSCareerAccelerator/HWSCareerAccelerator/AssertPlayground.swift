31d46832d6581872439598e6f5bf33b110948253
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-15 16:04:08 -0400

    add AssertPlayground to HWSCareerAccelerator

---
+++ HWSCareerAccelerator/HWSCareerAccelerator/AssertPlayground.swift
@@ -0,0 +1,18 @@
+//
+//  AssertPlayground.swift
+//  HWSCareerAccelerator
+//
+//  Created by Thomas Cowern on 10/15/24.
+//
+
+import SwiftUI
+
+struct AssertPlayground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssertPlayground()
+}
