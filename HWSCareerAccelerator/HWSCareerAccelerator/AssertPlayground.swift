3869b0aaa9f080e39ddeb76a121e1a6177f6c440
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-15 16:33:19 -0400

    update AssertPlayground in HWSCareerAccelerator with sum and checkNumber functions

--- HWSCareerAccelerator/HWSCareerAccelerator/AssertPlayground.swift
+++ HWSCareerAccelerator/HWSCareerAccelerator/AssertPlayground.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct AssertPlayground: View {
+    
+    @State private var answer: Int = 0
+    let number = Int.random(in: 1...6)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Test numbers") {
+//            answer = sum(of: [])
+            print("The number is \(number)")
+            checkNumber(number: number)
+        }
+    }
+    
+    func sum(of numbers: [Int]) -> Int {
+        assert(numbers.count > 0, "This should always be given some numbers to sum.")
+        return numbers.reduce(0, +)
+    }
+    
+    func checkNumber(number: Int) {
+        switch number {
+        case 1:
+            print("You rolled a one!")
+        case 2:
+            print("You rolled a two!")
+        case 3:
+            print("You rolled a three!")
+        default:
+            assertionFailure("Rolling a 3-sided dice shouldn't have any other results.")
+        }
     }
 }
 
