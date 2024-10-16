ba3d82c541bdb5d7d8af50f4b6170c565deba081
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-16 11:05:11 -0400

    update ContentView, add image to assets

--- SwiftUICampOct2024/SwiftUICampOct2024/ContentView.swift
+++ SwiftUICampOct2024/SwiftUICampOct2024/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var privateData = "Hello World"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text(privateData)
+            Button("Tap me") {
+                privateData = "Hello SwiftUI camp"
+            }
+            
+            // Pull in view example
+//            Text("Hi, I'm a preview!")
+//                .background(Color.red)
+            
+            // Push out view example
+//            Color.blue
+//            Color.green
         }
         .padding()
     }
