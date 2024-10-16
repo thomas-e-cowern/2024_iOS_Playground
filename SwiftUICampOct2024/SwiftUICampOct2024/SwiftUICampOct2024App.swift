a06fb0b49ee33e6217e8815ba61f94962e19b7a1
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-16 10:14:09 -0400

    SwiftUICampOct2024 initial commit

---
+++ SwiftUICampOct2024/SwiftUICampOct2024/SwiftUICampOct2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICampOct2024App.swift
+//  SwiftUICampOct2024
+//
+//  Created by Thomas Cowern on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICampOct2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
