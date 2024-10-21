f523d61e80d1c71e0c9cc1f8d744a378cfd7d290
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-21 16:06:26 -0400

    add DestinationView, update content view with desintationLink, update App with NavigationStack

---
+++ NavigationAnimationsPlayground/NavigationAnimationsPlayground/DestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationView.swift
+//  NavigationAnimationsPlayground
+//
+//  Created by Thomas Cowern on 10/21/24.
+//
+
+import SwiftUI
+
+struct DestinationView: View {
+    var body: some View {
+        Text("Destination View")
+    }
+}
+
+#Preview {
+    DestinationView()
+}
