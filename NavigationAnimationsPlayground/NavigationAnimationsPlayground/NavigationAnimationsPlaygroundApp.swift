4c1c2eeeab988aab1dfeb76a911b9579674c5dd3
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-21 16:00:15 -0400

    NavigationAnimationsPlayground initial commit

---
+++ NavigationAnimationsPlayground/NavigationAnimationsPlayground/NavigationAnimationsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAnimationsPlaygroundApp.swift
+//  NavigationAnimationsPlayground
+//
+//  Created by Thomas Cowern on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAnimationsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
