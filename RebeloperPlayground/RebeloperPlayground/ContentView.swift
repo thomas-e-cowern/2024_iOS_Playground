09a1c042e61ac6e5190e848c86bd09509223c710
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-22 13:38:15 -0400

    RebeloperPlayground initial commit

---
+++ RebeloperPlayground/RebeloperPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RebeloperPlayground
+//
+//  Created by Thomas Cowern on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RebeloperPlayground/RebeloperPlayground/RebeloperPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebeloperPlaygroundApp.swift
+//  RebeloperPlayground
+//
+//  Created by Thomas Cowern on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RebeloperPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
