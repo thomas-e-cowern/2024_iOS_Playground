69566b0a24679c059f976252a2a3b6583dffa908
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-22 19:16:32 -0400

    update Root1View with button navigation

--- RebeloperPlayground/RebeloperPlayground/Root1View.swift
+++ RebeloperPlayground/RebeloperPlayground/Root1View.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct Root1View: View {
+    
+    @State private var isSheetPresented: Bool = false
+    
     var body: some View {
         List {
             NavigationLink(value: Root1NavigationLinkValues.destination1) {
                 Text("Destination 1")
             }
+            
+            Button("Sheet Navigation") {
+                isSheetPresented.toggle()
+            }
+            .sheet(isPresented: $isSheetPresented) {
+                Root1NavigationLinkValues.destination1
+                    .navigationLinkValues(Root1NavigationLinkValues.self)
+            }
         }
+        .navigationTitle("Root 1 View")
     }
 }
