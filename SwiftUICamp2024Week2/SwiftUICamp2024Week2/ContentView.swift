404d9538f588e07aee735e93ae7af75063c30d89
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-10-23 10:08:13 -0400

    SwiftUICamp2024Week2 initial commit

---
+++ SwiftUICamp2024Week2/SwiftUICamp2024Week2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICamp2024Week2
+//
+//  Created by Thomas Cowern on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICamp2024Week2/SwiftUICamp2024Week2/SwiftUICamp2024Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICamp2024Week2App.swift
+//  SwiftUICamp2024Week2
+//
+//  Created by Thomas Cowern on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICamp2024Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
