0bae89755825e70daa04e76279de2db3e827e6a4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-12-09 15:10:25 -0500

    RebDeveloper Inital Commit

---
+++ RebDeveloper/RebDeveloper/RebDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebDeveloperApp.swift
+//  RebDeveloper
+//
+//  Created by Thomas Cowern on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RebDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
