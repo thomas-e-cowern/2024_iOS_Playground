3c0245967cd371b0f45f87974e23beb0b69234f6
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-12-09 16:12:17 -0500

    update Page to fix horizaontal scroll issue and removed scroll indicator

--- RebDeveloper/RebDeveloper/MultiScroll/Page.swift
+++ RebDeveloper/RebDeveloper/MultiScroll/Page.swift
@@ -29,15 +29,17 @@ struct Page: View {
             }
             .frame(height: geometry.size.height - 100)
             
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 0) {
-                    ForEach(0..<colors.count, id: \.self) { index in
-                        Cell(colors[index])
-                            .frame(width: 160, height: 100)
+            ScrollView(.vertical) {
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 0) {
+                        ForEach(0..<colors.count, id: \.self) { index in
+                            Cell(colors[index])
+                                .frame(width: 160, height: 100)
+                        }
                     }
                 }
+                .scrollIndicators(.hidden)
             }
-            
         }
     }
 }
