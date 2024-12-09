e14a7247084a0968e46e370927cd98436e67801c
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-12-09 15:15:50 -0500

    add MultiScroll folder, add Cell view

---
+++ RebDeveloper/RebDeveloper/MultiScroll/Cell.swift
@@ -0,0 +1,18 @@
+//
+//  Cell.swift
+//  RebDeveloper
+//
+//  Created by Thomas Cowern on 12/9/24.
+//
+
+import SwiftUI
+
+struct Cell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cell()
+}
