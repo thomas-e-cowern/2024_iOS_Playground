4242ed55c512343955b417f50a291c8fb2969896
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-12-11 16:21:33 -0500

    update coordinator and coordinatorstack

--- RebDeveloper/RebDeveloper/Coordinator/CoordinatorStack/Coordinator.swift
+++ RebDeveloper/RebDeveloper/Coordinator/CoordinatorStack/Coordinator.swift
@@ -5,3 +5,50 @@
 //  Created by Thomas Cowern on 12/11/24.
 //
 
+import SwiftUI
+
+@Observable
+class Coordinator<CoordinatorPage: Coordinatable> {
+    
+    var path: NavigationPath = NavigationPath()
+    var sheet: CoordinatorPage?
+    var fullScreenCover: CoordinatorPage?
+    
+    enum PushType {
+        case link
+        case sheet
+        case fullScreenCover
+    }
+    
+    enum PopType {
+        case link(last: Int)
+        case sheet
+        case fullScreenCover
+    }
+    
+    func push(_ page: CoordinatorPage, type: PushType = .link) {
+        switch type {
+        case .link:
+            path.append(page)
+        case .sheet:
+            sheet = page
+        case .fullScreenCover:
+            fullScreenCover = page
+        }
+    }
+    
+    func pop(type: PopType = .link(last: 1)) {
+        switch type {
+        case .link(let last):
+            path.removeLast(last)
+        case .sheet:
+            sheet = nil
+        case .fullScreenCover:
+            fullScreenCover = nil
+        }
+    }
+    
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+}
--- RebDeveloper/RebDeveloper/Coordinator/CoordinatorStack/CoordinatorStack.swift
+++ RebDeveloper/RebDeveloper/Coordinator/CoordinatorStack/CoordinatorStack.swift
@@ -5,3 +5,10 @@
 //  Created by Thomas Cowern on 12/11/24.
 //
 
+import SwiftUI
+
+struct CoordinatorStack<CoordinatorPage: Coordinatable>: View {    
+    var body: some View {
+        coordinator?.fullScreenCover() ?? EmptyView()
+    }
+}
