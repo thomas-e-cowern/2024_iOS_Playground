657090ac9f6fa0460d3e133c653a74a23c87116a
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-12-12 12:50:53 -0500

    update VSContentView to connect all the pieces

--- RebDeveloper/RebDeveloper/ViewStates/VSContentView.swift
+++ RebDeveloper/RebDeveloper/ViewStates/VSContentView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct VSContentView: View {
+    
+    @State private var viewModel: ViewModel = .init()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            switch viewModel.viewState {
+            case .loading:
+                ProgressView()
+                    .redacted(reason: .placeholder)
+                    .task {
+                        await viewModel.load()
+                    }
+            case let .loaded(profiles):
+                ProfilesListView(profiles: profiles)
+            case let .empty(message):
+                ViewStateEmptyView(message: message)
+            case let .error(message):
+                ViewStateErrorView(message: message) {
+                    viewModel.viewState = .loading
+                }
+            }
+        }
     }
 }
 
 #Preview {
     VSContentView()
+}
+
+fileprivate extension VSContentView {
+    @Observable
+    class ViewModel {
+        var viewState: LoadingViewState<[VSProfile]> = .loading
+        let manager = ProfileManager()
+        
+        func load() async {
+            do {
+                let profiles = try await manager.load()
+                if profiles.isEmpty {
+                    viewState = .empty("No profiles found")
+                } else {
+                    viewState = .loaded(profiles)
+                }
+            } catch {
+                viewState = .error(error.localizedDescription)
+            }
+        }
+    }
 }
